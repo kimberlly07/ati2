23ad19d276f6f2366a2288afb214759da06303b4
kimberlly07 <142352722+kimberlly07@users.noreply.github.com>
2024-10-04 11:32:46 -0300

    djl

--- main.js
+++ main.js
@@ -22,5 +22,3 @@
 // document.write(mediaTresvalores);
 let estados=
 ['Distrito Federal','Goiás','Mato Grosso','Mato Grosso do Sul','Alagoas','Bahia','Ceará','Maranhão','Paraíba','Pernambuco','Piauí','Rio Grande do Norte','Sergipe','Acre','Amapá','Amazonas','Pará','Rondônia','Roraima','Tocantis','Espírito Santo','Minas Gerais','Rio de Janeiro','São Paulo','Paraná','Rio Grande do Sul','Santa Catarina'];
-estados.length
-estados[22];
